1f3f6278e4e88079e5961252fe7e1db95137e4cb
Oleg <logardyt@gmail.com>
2025-04-22 11:43:01 +0700

    django_74

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", home, name="home"),
-    path("", contacts, name="contacts")
 ]
